bfbcf2ac1f533595b86ae24ea392d8603d07ee38
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-19 15:24:26 -0600

    Ejercicio 4 agregado

---
+++ Actividad 15_04/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int año = 0;
+int fecha = DateTime.Now.Year;
+
+do
+{
+    Console.WriteLine("Ingrese su año de nacimiento: ");
+
+    if (!int.TryParse(Console.ReadLine(), out año) || año < 1900 || año > fecha)
+    {
+        Console.WriteLine("Ingrese un año valido");
+        año = 0;
+    }
+} while (año == 0);
+
+Console.WriteLine("Ingreso valido: " + año);
+
+Console.ReadKey();
