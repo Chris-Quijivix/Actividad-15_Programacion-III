a58d37bc932fca06cb7279875523d6ee344cf5a8
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-19 11:16:58 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo de manejo de errores/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int edad = 0;
+
+do
+{
+    Console.WriteLine("Ingrese una edad:");
+    if(!int.TryParse(Console.ReadLine(), out edad))
+    {
+        Console.WriteLine("edad ingresado no valida");
+        edad = 0;
+    }
+    else if(edad <= 0 || edad > 100 )
+    {
+        Console.WriteLine("edad ingresada supera rango establecido");
+        edad = 0;
+    }
+}
+while (edad == 0);
+
+Console.WriteLine("edad: " + edad);
+
+Console.ReadKey();
