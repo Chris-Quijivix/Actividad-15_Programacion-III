7b0a9f9beaf91ab14a8410ca1acf2814d70534c4
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-19 15:30:26 -0600

    Ejercicio 5 agregado

---
+++ Actividad 15_05/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int numero;
+string ingreso;
+
+do
+{
+    Console.WriteLine("Ingrese un numero: ");
+    ingreso = Console.ReadLine();
+
+    if (!int.TryParse(ingreso, out numero))
+    {
+        Console.WriteLine("Ingreso de numero no valido");
+    }
+
+}while (!int.TryParse(ingreso, out numero));
+
+if (numero % 2  == 0)
+{
+    Console.WriteLine("El numero ingresado es par: " + numero);
+}
+else
+{
+    Console.WriteLine("El numero ingresado es impar: " + numero);
+}
+
+Console.ReadKey();
