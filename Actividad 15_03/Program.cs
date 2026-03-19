4ded0574e31724e18fcd631d87b528bfacb376fd
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-19 15:18:14 -0600

    Ejercicio 3 agregado

---
+++ Actividad 15_03/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int cantidad = 0;
+
+do
+{
+    Console.WriteLine("Cuantos estudiantes va a ingresar: ");
+
+    if (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+    {
+        Console.WriteLine("Ingreso invalido");
+        cantidad = 0;
+    }
+}while (cantidad == 0);
+
+Console.WriteLine("Ingreso de estudiantes valido, cantidad: " + cantidad);
+
+Console.ReadKey();
