9440c8610e27a418a1ec1363e7058cf98a775ab9
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-19 14:33:24 -0600

    Ejercicio 2 agregado

---
+++ Actividad 16_02/Program.cs
@@ -0,0 +1,17 @@
+﻿
+double precio  = 0;
+
+do
+{
+    Console.WriteLine("Ingrese el precio del producto: ");
+
+    if (!double.TryParse(Console.ReadLine(), out precio) || precio  <= 0)
+    {
+        Console.WriteLine("Ingreso no valido");
+        precio = 0;
+    }
+}while (precio == 0);
+
+Console.WriteLine("Precio ingresado valido, es: " + precio);
+
+Console.ReadKey();
