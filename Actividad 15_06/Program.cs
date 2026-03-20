d5f839b09f6c540be90c881dab72a3668e488256
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-19 22:04:33 -0600

    Ejercicio 6 agregado

---
+++ Actividad 15_06/Program.cs
@@ -0,0 +1,16 @@
+﻿DateTime fecha;
+
+do
+{
+    Console.WriteLine("Ingrese una fecha: ");
+
+    if (!DateTime.TryParse(Console.ReadLine(), out fecha))
+    {
+        Console.WriteLine("Fecha invalida");
+        fecha = DateTime.Now;
+    }
+}while (fecha == DateTime.MinValue);
+
+Console.WriteLine("La fecha ingresada es: " + fecha.ToShortDateString());
+
+Console.ReadKey();
