897387afaf6cb4049f2f4ce91e8129fbe3b27370
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-19 22:17:58 -0600

    Ejercicio 9 agregado

---
+++ Actividad 15_09/Program.cs
@@ -0,0 +1,24 @@
+﻿
+double nota = -1;
+
+do
+{
+    Console.WriteLine("Ingrese una nota: ");
+
+    if (!double.TryParse(Console.ReadLine(), out nota) || nota < 0 || nota > 100)
+    {
+        Console.WriteLine("Ingrese una nota valida");
+        nota = -1;
+    }
+}while (nota < 0);
+
+if (nota >= 61)
+{
+    Console.WriteLine("Aprobado");
+}
+else
+{
+    Console.WriteLine("Reprobado");
+}
+
+Console.ReadKey();
