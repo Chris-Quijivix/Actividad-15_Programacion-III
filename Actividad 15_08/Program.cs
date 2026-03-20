cf6ea1ac27c847571320285cc8a86346db1d9304
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-19 22:13:17 -0600

    Ejercicio 8 agregado

---
+++ Actividad 15_08/Program.cs
@@ -0,0 +1,23 @@
+﻿
+double numero;
+
+do
+{
+    Console.WriteLine("Ingrese un numero: ");
+
+    if (!double.TryParse(Console.ReadLine(), out numero))
+    {
+        Console.WriteLine("Numero ingresado no valido");
+        numero = -1;
+    }
+    else if (numero < 0)
+    {
+        Console.WriteLine("Ingrese numeros positivos unicamente");
+    }
+} while (numero < 0);
+
+double raiz = numero * 0.5;
+
+Console.WriteLine("La raiz cuadrada del numero ingresado es: " + raiz);
+
+Console.ReadKey();
