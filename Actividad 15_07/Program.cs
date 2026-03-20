acea62d915fe64ec5b72c6401afb8e27e3a24a36
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-19 22:08:06 -0600

    Ejercicio 7 agregado

---
+++ Actividad 15_07/Program.cs
@@ -0,0 +1,17 @@
+﻿
+double salario = -1;
+
+do
+{
+    Console.WriteLine("Ingrese el salario: ");
+
+    if (!double.TryParse(Console.ReadLine(), out salario) || salario < 0)
+    {
+        Console.WriteLine("Ingrese un salario valido");
+        salario = -1;
+    }
+}while (salario <  0);
+
+Console.WriteLine("El salario ingresado es: " + salario);
+
+Console.ReadKey();
